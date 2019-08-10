a701f867bad923628937e800971c463deea42540
Roman Kiss <romiki44@hotmail.com>
2019-08-10 17:28:57 +0200

    lec472 done

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,25 +7,64 @@ import Search from './Search';
 const Ingredients=()=> {
   const [userIngredients, setUserIngredients]=useState([]);
 
+  useEffect(()=>{
+    fetch('https://react-hooks-demo-6e6a5.firebaseio.com/ingredients.json')
+    .then(response=>response.json())
+    .then(responseData=>{
+      const loadedIngredients=[];
+      for(const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      };
+      setUserIngredients(loadedIngredients);
+    });
+  }, []);
+
+  useEffect(()=>{
+    console.log('RENDERING INGREDIENTS', userIngredients);
+  },[userIngredients]);
+
+  const filteredIngredientsHandler=useCallback(filteredIngredients=>{
+    setUserIngredients(filteredIngredients);
+  },[]);
+
   const addIngredientHandler=ingredient=>{
-    setUserIngredients(prevIngredients=>[
-      ...prevIngredients, 
-      {id: Math.random().toString(),...ingredient}
-    ]);
+    fetch('https://react-hooks-demo-6e6a5.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(response=>{
+      return response.json();
+    })
+    .then(responseData=>{
+      setUserIngredients(prevIngredients=>[
+        ...prevIngredients, 
+        {id: responseData.name,...ingredient}
+      ]);
+    });
   };
 
-  const removeIngredientHandler=(ingredientId=>{
-    console.log('removing', ingredientId);
+const removeIngredientHandler=(ingredientId=>{
+  console.log('removing', ingredientId);
+  fetch(`https://react-hooks-demo-6e6a5.firebaseio.com/ingredients/${ingredientId}.json`, {
+    method: 'DELETE'
+  })
+  .then(response=>{
     setUserIngredients(prevIngredients=>
       prevIngredients.filter(ingredient=>ingredient.id !== ingredientId))
     });
-
+  });
+    
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
         <IngredientList ingredients={userIngredients} onRemoveItem={(id)=>{removeIngredientHandler(id)}}/>
       </section>
     </div>
